f411dcb1cb3dddc98a610ff78ab3869af5a8f9f7
Aman <aman.gupta@adsandurl.com>
2021-12-13 13:35:39 +0530

    CartActivity: added pay visit bottomsheet

--- app/src/main/java/com/petroll/ui/cart/CartActivity.kt
+++ app/src/main/java/com/petroll/ui/cart/CartActivity.kt
@@ -2,13 +2,16 @@ package com.petroll.ui.cart
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.petroll.R
 import com.petroll.databinding.ActivityCartBinding
+import com.petroll.databinding.ConfirmOrderBottomsheetBinding
 import com.petroll.ui.cart.fragments.AddressFragment
 import com.petroll.ui.cart.fragments.CartFragment
 import com.petroll.ui.cart.fragments.PaymentFragment
@@ -17,8 +20,8 @@ import com.petroll.utils.BaseActivity
 
 class CartActivity : BaseActivity() {
 
+    lateinit var dialog: BottomSheetDialog
     lateinit var binding: ActivityCartBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,28 +36,34 @@ class CartActivity : BaseActivity() {
         binding.back.tvBack.text = getString(R.string.cart_details)
         setNextClick()
         setUpViewPager()
+
+        confirmOrderBottomSheet(layoutInflater)
     }
 
     fun setNextClick() {
-
         baseBind.rlNext.setOnClickListener {
             when (binding.vpFragments.currentItem) {
                 0 -> {
-                    binding.vpFragments.currentItem = 1
-                    setBottomBarText(getString(R.string.checkout))
-                    binding.back.tvBack.text = getString(R.string.address)
-                    binding.topProgress.ivAddress.setColorFilter(ContextCompat.getColor(this,R.color.indicator_highlight))
-                    binding.topProgress.tvAddress.setTextColor(ContextCompat.getColor(this, R.color.indicator_highlight))
+                    dialog.show()
                 }
                 1 -> {
                     binding.vpFragments.currentItem = 2
                     binding.back.tvBack.text = getString(R.string.payment)
                     setBottomBarText(getString(R.string.place_order))
-                    binding.topProgress.ivPayment.setColorFilter(ContextCompat.getColor(this,R.color.indicator_highlight))
-                    binding.topProgress.tvPayment.setTextColor(ContextCompat.getColor(this, R.color.indicator_highlight))
+                    binding.topProgress.ivPayment.setColorFilter(
+                        ContextCompat.getColor(
+                            this,
+                            R.color.indicator_highlight
+                        )
+                    )
+                    binding.topProgress.tvPayment.setTextColor(
+                        ContextCompat.getColor(
+                            this,
+                            R.color.indicator_highlight
+                        )
+                    )
                 }
                 else -> {
-                    // todo nothing
                     val intent = Intent(this, OrderPlacedActivity::class.java)
                     startActivity(intent)
                     finish()
@@ -77,26 +86,46 @@ class CartActivity : BaseActivity() {
                 }
                 1 -> {
                     binding.vpFragments.currentItem = 0
-                    binding.back.tvBack.text = getString(R.string.your_cart)
+                    binding.back.tvBack.text = getString(R.string.cart_details)
                     setBottomBarText(getString(R.string.confirm_order))
 //                    backButtonHandling()
                     setNextClick()
-                    binding.topProgress.ivAddress.setColorFilter(ContextCompat.getColor(this,R.color.line_color))
-                    binding.topProgress.tvAddress.setTextColor(ContextCompat.getColor(this, R.color.top_text_color))
+                    binding.topProgress.ivAddress.setColorFilter(
+                        ContextCompat.getColor(
+                            this,
+                            R.color.line_color
+                        )
+                    )
+                    binding.topProgress.tvAddress.setTextColor(
+                        ContextCompat.getColor(
+                            this,
+                            R.color.top_text_color
+                        )
+                    )
                 }
                 else -> {
                     binding.vpFragments.currentItem = 1
                     binding.back.tvBack.text = getString(R.string.address)
                     setBottomBarText(getString(R.string.checkout))
-                    binding.topProgress.ivPayment.setColorFilter(ContextCompat.getColor(this,R.color.line_color))
-                    binding.topProgress.tvPayment.setTextColor(ContextCompat.getColor(this, R.color.top_text_color))
+                    binding.topProgress.ivPayment.setColorFilter(
+                        ContextCompat.getColor(
+                            this,
+                            R.color.line_color
+                        )
+                    )
+                    binding.topProgress.tvPayment.setTextColor(
+                        ContextCompat.getColor(
+                            this,
+                            R.color.top_text_color
+                        )
+                    )
 
                 }
             }
         }
     }
 
-    fun setUpViewPager() {
+    private fun setUpViewPager() {
         val adapter = MyPageAdapter(supportFragmentManager)
         binding.vpFragments.adapter = adapter
 
@@ -118,6 +147,40 @@ class CartActivity : BaseActivity() {
                 1 -> AddressFragment()
                 else -> PaymentFragment()
             }
+        }
+    }
+
+    private fun confirmOrderBottomSheet(inflater: LayoutInflater) {
+        dialog = BottomSheetDialog(this)
+        val confirmOrderBinding: ConfirmOrderBottomsheetBinding =
+            ConfirmOrderBottomsheetBinding.inflate(inflater)
+        dialog.setContentView(confirmOrderBinding.root)
+
+        confirmOrderBinding.payAtVisit.setOnClickListener {
+            dialog.dismiss()
+            val intent = Intent(this, OrderPlacedActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        confirmOrderBinding.other.setOnClickListener {
+            dialog.dismiss()
+
+            binding.vpFragments.currentItem = 1
+            setBottomBarText(getString(R.string.checkout))
+            binding.back.tvBack.text = getString(R.string.address)
+            binding.topProgress.ivAddress.setColorFilter(
+                ContextCompat.getColor(
+                    this,
+                    R.color.indicator_highlight
+                )
+            )
+            binding.topProgress.tvAddress.setTextColor(
+                ContextCompat.getColor(
+                    this,
+                    R.color.indicator_highlight
+                )
+            )
         }
     }
 }
